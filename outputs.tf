606ac35e66ab3bb753cc5434922d8e11ae718c73
grahamlyons <graham@grahamlyons.com>
2017-10-03 14:00:03 +0100

    Fix the output from this module
    The ternary conditional evaluates every expression so the missing
    resource causes it to fail, which happens silently in an `output`.
    
    This uses the fact that the resources have a `count` to treat them
    as lists, `concat` them to one list, `compact` them to remove empty
    elements, then take the first (and only) element from the list.
    
    JIRA: PLAT-1117

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "fqdn" {
-  value = "${var.alb_zone_id == "" ? ${aws_route53_record.dns_record.fqdn} : ${aws_route53_record.alb_alias.fqdn}}"
+  value = "${element(
+    compact(
+        concat(
+            aws_route53_record.dns_record.*.fqdn,
+            aws_route53_record.alb_alias.*.fqdn
+        )
+    ), 0)}"
 }
