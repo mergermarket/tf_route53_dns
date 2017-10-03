4301465b86bc751101bc765b1c280db23edfdc1a
grahamlyons <graham@grahamlyons.com>
2017-10-03 12:23:46 +0100

    Conditionally output correct FQDN
    The module can create different resources so the `dns_record` may
    not always be available, so the `fqdn` output isn't availble either.
    
    This outputs the `fqdn` from the `alb_alias` if that is created.
    
    JIRA: PLAT-1117

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "fqdn" {
-  value = "${aws_route53_record.dns_record.fqdn}"
+  value = "${var.alb_zone_id == "" ? ${aws_route53_record.dns_record.fqdn} : ${aws_route53_record.alb_alias.fqdn}}"
 }
